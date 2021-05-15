11ab3f13a0d16369071e179f1ebe5c2932d4935a
BodNS <bondarenokn@gmail.com>
2021-05-15 11:44:13 +0300

    animal controller was updated

--- controllers/animal.controller.js
+++ controllers/animal.controller.js
@@ -14,8 +14,47 @@ module.exports.createAnimal = async(req, res, next) => {
     }
 }
 module.exports.getAnimal = async(req, res, next) => {
-    
+    const {params: {animalId}} = req;
+    try {
+        const foundAnimal = await Animal.findById(animalId);
+        return res.status(201).send({data: foundAnimal});
+    } catch (err){
+        next(err);
+    }
 };
 
 
-module.exports.getAllAnimal = async(req, res, next) => {};
+module.exports.getAllAnimal = async(req, res, next) => {
+    try {
+        const foundAnimals = await Animal.find();
+        return res.status(201).send({data: foundAnimals});
+    } catch (err){
+        next(err);
+    }
+};
+module.exports.updateAnimal = async (req, res, next) => {
+    const {body, params: {animalId}} = req;
+    try{
+        const updateAnimal = await Animal.findByIdAndUpdate(animalId, body);
+        console.log('update animal', updateAnimal);
+        if(updateAnimal){
+            return res.status(200).send({data: updateAnimal});
+        }
+        res.status(400).send('Bad request');
+    } catch (err){
+        next(err);
+    }
+};
+
+module.exports.deleteAnimal = async (req, res, next) => {
+    const {params: {animalId}} = req;
+    try{
+        const deleteAnimal = await Animal.findByIdAndDelete(animalId);
+        if(deleteAnimal){
+            return res.status(201).send({data: deleteAnimal});
+        }
+        res.status(404).send('Animal not found');
+    } catch (err){
+        next(err);
+    }
+};
